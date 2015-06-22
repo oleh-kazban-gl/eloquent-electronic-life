466da11bb30b927196da26390605774194b0b3cd
Oleh Kazban <oleh.kazban@globallogic.com>
2015-06-22 17:38:56 +0300

    Update with simple modular system

--- simpleEcosystem.js
+++ simpleEcosystem.js
@@ -1,3 +1,57 @@
-/**
- * Created by oleh.kazban on 6/19/2015.
- */
+function Wall() {
+}
+
+function Plant() {
+  this.energy = 3 + Math.random() * 4;
+}
+
+Plant.prototype.act = function (context) {
+  if (this.energy > 15) {
+    var space = context.find(' ');
+    if (space)
+      return {type: 'reproduce', direction: space};
+  }
+  if (this.energy < 20)
+    return {type: 'grow'};
+};
+
+function PlantEater() {
+  this.energy = 20;
+}
+
+PlantEater.prototype.act = function (context) {
+  var space = context.find(' ');
+  if (this.energy > 60 && space)
+    return {type: 'reproduce', direction: space};
+  var plant = context.find('*');
+  if (plant)
+    return {type: 'eat', direction: plant};
+  if (space)
+    return {type: 'move', direction: space};
+};
+
+function WallFollower() {
+  this.dir = 's';
+}
+
+WallFollower.prototype.act = function (view) {
+  var start = this.dir;
+  if (view.look(dirPlus(this.dir, -3)) != ' ')
+    start = this.dir = dirPlus(this.dir, -2);
+  while (view.look(this.dir) != ' ') {
+    this.dir = dirPlus(this.dir, 1);
+    if (this.dir == start) break;
+  }
+  return {type: 'move', direction: this.dir};
+};
+
+function BouncingCritter() {
+  this.direction = straightElement(GridModule.directionNames);
+};
+
+BouncingCritter.prototype.act = function (view) {
+  if (view.look(this.direction) != ' ')
+    this.direction = view.find(' ') || 's';
+  return {type: 'move', direction: this.direction};
+};
+
